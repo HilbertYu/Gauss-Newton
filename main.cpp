11f0443a6f6ff3f57a4fd6a4e1455e2e615cefba
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2019-04-18 12:52:23 +0800

    * show err

--- main.cpp
+++ main.cpp
@@ -18,13 +18,17 @@ class GaussNewton
 {
     double g(double x, RowVector b)
     {
-     //   double A = b(0);
+        //double A = b(0);
         double mu = b(1);
-        double s = b(2);
-        double ret = -(x - mu)*(x - mu)/(2*s*s);
+        double r = b(2);
+
+
+        double ret = 1 + pow((x - mu),2)/(r*r);
+
         return ret;
     }
 
+#if 1
     double g_mu(double x, RowVector b)
     {
         double mu = b(1);
@@ -39,6 +43,7 @@ class GaussNewton
         double s = b(2);
         return (1.0/(s*s*s))*(x - mu)*(x - mu);
     }
+#endif
 public:
     class Point
     {
@@ -59,7 +64,7 @@ public:
 
     double f(double x, RowVector b)
     {
-        return b(0) * exp(g(x, b));
+        return b(0)/(b(2)*g(x, b));
     }
 
     ColumnVector res(RowVector b)
@@ -80,17 +85,38 @@ public:
     RowVector grad_f(double x, RowVector b)
     {
         double A = b(0);
-        //double mu = b(1);
-       // double s = b(2);
+        double mu = b(1);
+        double r = b(2);
 
         double f_A = f(x, b)/A;
-        double f_mu = f(x, b) * g_mu(x, b);
-        double f_s = f(x, b) * g_s(x, b);
+
+
+        //f-mu
+        double f_mu = 0;
+
+        {
+            double a = -A*(2*mu - 2*x);
+            double b1 = pow(r,3) * pow(g(x,b),2);
+            f_mu = a/b1;
+        }
+
+        //====
+
+        double f_r = 0;
+
+        double f_r_l = -f(x,b)/r;
+        {
+            double a = 2*A*pow(x - mu, 2);
+            double b1 = pow(r,4) * pow(g(x, b),2);
+            f_r = f_r_l + a/b1;
+        }
+
+
 
         RowVector ret_v(3);
         ret_v(0) = f_A;
         ret_v(1) = f_mu;
-        ret_v(2) = f_s;
+        ret_v(2) = f_r;
 
         return ret_v;
     }
@@ -114,7 +140,7 @@ public:
 
     RowVector run(RowVector b)
     {
-        const int max_iter = 20;
+        const int max_iter = 1000;
 
         for (int i = 0; i < max_iter; ++i)
         {
@@ -125,8 +151,11 @@ public:
             RowVector bn = T.solve(Jt*res(b));
 
             double err = sqrt(bn(0)*bn(0) + bn(1)*bn(1) + bn(2)*bn(2));
-        //    printf("err = %.5lf\n", err);
+            printf("err = %.8lf", err);
+            
+            std::cout << bn;
 
+            printf("\n");
             b = b + bn;
         }
 
